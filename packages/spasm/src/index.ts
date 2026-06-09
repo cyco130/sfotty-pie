161acbb1643d2ea219b03c9db903da322bfaba72
Fatih Aygün <cyco130@gmail.com>
2026-06-09 12:01:55 +0100

    feat(spasm): add a spasm CLI (spasm INPUT -o OUTPUT) over an async fs host

--- packages/spasm/src/index.ts
+++ packages/spasm/src/index.ts
@@ -1,3 +1,4 @@
 export { assemble, type AssembleResult } from "./assemble.ts";
+export type { Host } from "./loader.ts";
 export type { Message } from "./parser.ts";
 export type { Value } from "./value.ts";
